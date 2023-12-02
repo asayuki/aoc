b1af637803dda6bf595385545cc1b6d8a152f77f
Erik Thunberg <erik@streamify.io>
2023-12-02 11:24:57 +0100

    Day 2: Cube Conundrum

---
+++ year/2023/02/index.js
@@ -0,0 +1,44 @@
+export class Day {
+  constructor(input) {
+    this.title = 'Day 2: Cube Conundrum'
+    this.input = input.split(/\n/);
+    this.rules = {
+      red: 12,
+      green: 13,
+      blue: 14,
+    };
+  }
+
+  solution1() {
+    return this.input.reduce((validGames, game) => {
+      let valid = true;
+      const [g, c] = game.split(': ');
+      const [, id] = g.split(' ');
+      const cubes = c.split(/[;,]+/)
+      
+      cubes.forEach((cube) => {
+        const [num, color] = cube.trim().split(' ');
+        if (num > this.rules[color]) {
+          valid = false;
+        }
+      });
+
+      return valid ? validGames += Number(id) : validGames;
+    }, 0);
+  }
+
+  solution2() {
+    return this.input.reduce((totalPower, game) => {
+      let min = { red: 0, green: 0, blue: 0 };
+      const [g, c] = game.split(': ');
+      const cubes = c.split(/[;,]+/)
+      cubes.forEach((cube) => {
+        const [num, color] = cube.trim().split(' ');
+        if (Number(num) > min[color]) {
+          min[color] = Number(num);
+        }
+      });
+      return totalPower + Object.keys(min).reduce((current, color) => min[color] !== 0 ? current !== 0 ? current * min[color] : current + min[color] : current, 0);
+    }, 0);
+  }
+}
