ef533f67e9a49d8099ae4896353180d9650f1c36
Erik Thunberg <erik.thunberg@adeprimo.se>
2025-12-03 12:35:38 +0100

    Day 3: Lobby - Part 2

--- year/2025/3/index.js
+++ year/2025/3/index.js
@@ -6,7 +6,7 @@ export class Day {
 
     solution1() {
         return this.input.reduce((largest, numbers) => largest + numbers.reduce((c, n, i) => {
-            for (let j = i + 1; j < numbers.length; j++) {
+            for (let j = i + 1; j < numbers.length; j += 1) {
                 c = Number(`${n}${numbers[j]}`) > c ? Number(`${n}${numbers[j]}`) : c;
             }
             return c;
@@ -14,5 +14,24 @@ export class Day {
     }
 
     solution2() {
+        return this.input.reduce((largest, numbers) => {
+            let highestNumberCombination = '';
+            let lastIndex = -1;
+
+            for (let k = 0; k < 12; k += 1) {
+                let currentHighest = -1;
+                let currentIndex = -1;
+                for (let i = lastIndex + 1; i < numbers.length - (12 - k - 1); i += 1) {
+                    if (numbers[i] > currentHighest) {
+                        currentHighest = numbers[i];
+                        currentIndex = i;
+                    }
+                }
+                highestNumberCombination += currentHighest;
+                lastIndex = currentIndex;
+            }
+
+            return largest + Number(highestNumberCombination);
+        }, 0);
     }
 }
